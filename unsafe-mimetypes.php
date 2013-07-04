05c34544fdba9067e98e321afef23943fa2c60de
Luke Drummond <lukedrummond@gmail.com>
2013-07-04 23:08:40 +0100

    Thu  4 Jul 16:56:44 BST 2013

--- unsafe-mimetypes.php
+++ unsafe-mimetypes.php
@@ -44,7 +44,7 @@ function unsafe_mime_settings_page()
 	<div class="wrap">
 	    <?php screen_icon(); ?>
 	    <h2>Configure Custom Mimetypes</h2>	
-		<form method="post" action="options.php">
+		<form method="post" action="options-general.php">
 	<?php
 		settings_fields('unsafe-mime-group');
 		do_settings_sections('test-setting-admin');
