17a751ed48934f1e411aa7a4def4fc15b78fcf54
Luke Drummond <lukedrummond@gmail.com>
2013-07-04 17:16:43 +0100

    Thu  4 Jul 16:56:44 BST 2013

--- unsafe-mimetypes.php
+++ unsafe-mimetypes.php
@@ -1,4 +1,16 @@
 <?php
+/*
+Plugin Name: wordpress unsafe mimetypes
+Plugin URI: http://redmine.lukedrummond.net/projects/wp-unsafe-mimetypes
+Description: Allows users to add file types to the whitelist of allowed media formats.  This  is especially useful if you wish to distribute binaries, or specialist media formats (i.e. not just mp3, jpg and pdf)
+Version: 0.0.1
+Author: Luke Drummond
+Author URI: https://lukedrummond.net
+License: zlib
+*/
+?>
+
+<?php
 class wctest{
     public function __construct(){
         if(is_admin()){
