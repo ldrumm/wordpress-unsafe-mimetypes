907984c61294e97a910e1fe31d7d3ed1baa7aa26
Luke Drummond <lukedrummond@gmail.com>
2013-07-04 16:48:25 +0100

    ,

--- unsafe-mimetypes.php
+++ unsafe-mimetypes.php
@@ -14,27 +14,27 @@ License: zlib
 <?php
 /*Add the settings options to the wordpress admin menu*/
 
-function unsafe_mime_list_types()
-{
+#function unsafe_mime_list_types()
+#{
+#	
+#echo 'here are the settings yeah';
+#}
+
+#function unsafe_mime_commit_types()
+#{
+#echo 'here are the settings yeah';
+#}
 
 
-}
+#function unsafe_mime_register_types()
+#{
+#echo 'here are the settings yeah';
+#}
 
-function unsafe_mime_commit_types()
-{
-
-}
-
-
-function unsafe_mime_register_types()
-{
-
-}
-
-function unsafe_mime_settings_page()
-{
-	echo 'here are the settings yeah';
-}
+#function unsafe_mime_settings_page()
+#{
+#	echo 'here are the settings yeah';
+#}
 
 
 add_options_page('Add custom mime types', 'mimetypes', 'Administrator', 'mimetypes-settings', 'unsafe_mime_settings_page');
