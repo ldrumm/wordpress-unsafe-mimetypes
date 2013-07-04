e8fd32aedfdcd75b6ef1b9aa654e733bb93a0f2d
Luke Drummond <lukedrummond@gmail.com>
2013-07-04 18:13:01 +0100

    Thu  4 Jul 16:56:44 BST 2013

--- unsafe-mimetypes.php
+++ unsafe-mimetypes.php
@@ -49,7 +49,7 @@ function unsafe_mime_settings_page()
 	
 	<?php
 		settings_fields('unsafe-mime-group');
-		do_settings_sections('unsafe-mime-group');
+		do_settings_sections('setting_section_id');
 		submit_button(); 
 	?>
 	    </form>
