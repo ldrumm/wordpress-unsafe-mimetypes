974a350338c3198b5ccac034ea1b0b0dc1c44b96
Luke Drummond <lukedrummond@gmail.com>
2013-07-05 23:33:54 +0100

    Fri  5 Jul 19:57:40 BST 2013

--- unsafe-mimetypes.php
+++ unsafe-mimetypes.php
@@ -58,9 +58,9 @@ function unsafe_mime_settings_page()
 
 function unsafe_mime_ui_info()
 {
-	echo 'Configure which mimetypes you want your users to be able to upload.';
-	echo 'Choose whether all content editors, or just WordPress Administrators can upload the \'unsafe\' types.';
-	echo 'The current list of custom mimetypes is as follows:<br/><br/><em>' . get_option('unsafe_mime_settings_list') . '</em>';
+	echo 'Configure which mimetypes you want your users to be able to upload.<br/>';
+	echo 'Choose whether all content editors, or just WordPress Administrators can upload the \'unsafe\' types.<br/>';
+	echo 'The current list of custom mimetypes is as follows:<small><em>' . get_option('unsafe_mime_settings_list') . '</em></small>';
 }
 
 function unsafe_mime_ui_list_box()
