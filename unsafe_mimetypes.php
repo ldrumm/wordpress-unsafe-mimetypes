fe426a3a81f24e8116c165eaa8aefe37fd7c18c1
Luke Drummond <lukedrummond@gmail.com>
2013-07-06 01:15:14 +0100

    Sat  6 Jul 01:14:42 BST 2013

--- unsafe_mimetypes.php
+++ unsafe_mimetypes.php
@@ -37,10 +37,10 @@ function unsafe_mime_settings_page()
 		die(__("setting option not allowed"));
 	}
 	if(isset($_POST['mime_list'])){
-	    update_option('unsafe_mime_settings_list', sanitize_mime_type(strtolower($_POST['mime_list'])));
+	    update_option('unsafe_mime_settings_list', strtolower($_POST['mime_list']));
 	}
 	if(isset($_POST['mime_priv'])){
-		update_option('unsafe_mime_settings_priv', sanitize_text_field($_POST['mime_priv']));
+		update_option('unsafe_mime_settings_priv', $_POST['mime_priv']);
 	}
 	?>
 	
